92728f42aacda1d60eb6e3685d5de46f630c0615
Simon <guichardsim@gmail.com>
2021-11-06 19:03:43 +0100

    fix build

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,31 @@
+import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess({}),
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		// By default, `npm run build` will create a standard Node app.
+		// You can create optimized builds for different platforms by
+		// specifying a different adapter
+
+    adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html'
+		}),
+
+		// disable SSR
+		ssr: false,
+		// disable Prerendering
+		prerender: { enabled: true },
+
+		// hydrate the element in src/app.html
+		target: 'body',
+
+		vite: () => ({})
 	}
 };
 
