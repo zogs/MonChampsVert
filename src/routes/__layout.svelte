b45250aeefd054b883bfd10631cbb0a5da4b2517
Simon Guichard <guichardsim@gmail.com>
2021-11-07 12:14:50 +0100

    fix background

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -19,7 +19,7 @@
 
 <style>
   :global(html) { height:100%; width:100%; background: linear-gradient(#9199e4, #b8ffde); overflow:hidden; }
-  :global(body) { height:100%; width:100%;display:flex; justify-content:center; align-items:flex-start; overflow-x: hidden; background-image: url('images/smartphone.png'); background-repeat: no-repeat; background-position: 50% 150px }
+  :global(body) { height:100%; width:100%;display:flex; justify-content:center; align-items:flex-start; overflow-x: hidden; background-image: url('/images/smartphone.png'); background-repeat: no-repeat; background-position: 50% 150px }
   :global(body *)  { font-family: 'Comfortaa'; }
   main { position:absolute; top:0; left:0; right:0; bottom:0; display:flex; flex-direction:column; background-color:#9cd248; overflow-x: hidden; z-index:2;}
   main.desktop-mode { position:relative; top: 220px; width:370px; height:620px; }
